0ff5c5ac4691c8a96aacc8c38eae0ed052bcf6b0
shash0423 <shash042319@naver.com>
2024-03-28 14:11:03 +0900

    초기 커밋.

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+void InverseImage(BYTE* Img, BYTE* Out, int W, int H)
+{
+	int ImgSize = W * H;
+	for (int i = 0; i < ImgSize; i++)
+	{
+		Out[i] = 255 - Img[i];
+	}
+}
+
+void main()
+{
+	BITMAPFILEHEADER hf; // 14Bytes
+	BITMAPINFOHEADER hInfo; //40Bytes
+	RGBQUAD hRGB[256]; // (256 * 4Bytes)
+	FILE* fp;
+	fp = fopen("coin.bmp", "rb");
+	if (fp == NULL) return;
+	fread(&hf, sizeof(BITMAPFILEHEADER), 1, fp);
+	fread(&hInfo, sizeof(BITMAPINFOHEADER), 1, fp);
+	fread(hRGB, sizeof(RGBQUAD), 256, fp);
+	int ImgSize = hInfo.biWidth * hInfo.biHeight;
+	BYTE* Image = (BYTE*)malloc(ImgSize);
+	BYTE* Output = (BYTE*)malloc(ImgSize);
+	fread(Image, sizeof(BYTE), ImgSize, fp);
+	fclose(fp);
+
+	// 영상처리
+	InverseImage(Image, Output, hInfo.biWidth, hInfo.biHeight);
+
+	fp = fopen("output.bmp", "wb");
+	fwrite(&hf, sizeof(BYTE), sizeof(BITMAPFILEHEADER), fp);
+	fwrite(&hInfo, sizeof(BYTE), sizeof(BITMAPINFOHEADER), fp);
+	fwrite(hRGB, sizeof(RGBQUAD), 256, fp);
+	fwrite(Output, sizeof(BYTE), ImgSize, fp);
+	fclose(fp);
+	free(Image);
+	free(Output);
+}
