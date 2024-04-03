3a53a021f20e492f5a28450c357c476b6272a159
shash0423 <shash042319@naver.com>
2024-04-03 16:12:23 +0900

    화면 밝기를 유지하면서 라플라시안의 노이즈 민감도 확인

--- main.cpp
+++ main.cpp
@@ -279,6 +279,29 @@ void Laplace_Conv(BYTE* Img, BYTE* Out, int W, int H) {
 	}
 }
 
+// 라플라시안이 노이즈에 얼마나 민감한지 확인
+void Laplace_Conv_DC(BYTE* Img, BYTE* Out, int W, int H) {
+	double Kennel[3][3] = { -1.0,-1.0,-1.0,
+							-1.0,9.0,-1.0,
+							-1.0,-1.0,-1.0 };
+	double SumProduct = 0.0;
+	// margin을 두기 위해 1부터 시작해서 W(H) -1 전에 종료
+	for (int i = 1; i < H - 1; i++) {
+		for (int j = 1; j < W - 1; j++) { // 여기까지 center 화소를 나타냄
+			for (int m = -1; m <= 1; m++) {
+				for (int n = -1; n <= 1; n++) { // center 화소의 주변부를 계산하기 위한 for문 2개
+					SumProduct += Img[(i + m) * W + (j + n)] * Kennel[m + 1][n + 1];
+				}
+			}
+			
+			if (SumProduct > 255.0) Out[i * W + j] = 255;
+			else if (SumProduct < 0.0) Out[i * W + j] = 0;
+			else Out[i * W + j] = (BYTE)SumProduct;
+			SumProduct = 0.0;
+		}
+	}
+}
+
 void main()
 {
 	BITMAPFILEHEADER hf; // 14Bytes
@@ -317,7 +340,8 @@ void main()
 	//	 if (Temp[i] > Output[i]) Output[i] = Temp[i];
 	// }
 	// Binarization(Output, Output, hInfo.biWidth, hInfo.biHeight, 40);
-	Laplace_Conv(Image, Output, hInfo.biWidth, hInfo.biHeight);
+	// Laplace_Conv(Image, Output, hInfo.biWidth, hInfo.biHeight);
+	Laplace_Conv_DC(Image, Output, hInfo.biWidth, hInfo.biHeight);
 
 	// HistogramStretching(Image, Output, Histo, hInfo.biWidth, hInfo.biHeight);
 	// InverseImage(Image, Output, hInfo.biWidth, hInfo.biHeight);
