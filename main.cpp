ff853102b9acbad4f891fc2bab8ac98a467547e1
shash0423 <shash042319@naver.com>
2024-05-25 19:29:35 +0900

    침식

--- main.cpp
+++ main.cpp
@@ -658,13 +658,32 @@ void RGB2YCbCr(BYTE* Image, BYTE* Y, BYTE* Cb, BYTE* Cr, int W, int H) {
     }
 }
 
+// 축소
+void Erosion(BYTE* Image, BYTE* Output, int W, int H) {
+	for (int i = 1; i < H - 1; i++) {
+		for (int j = 1; j < W - 1; j++) {
+			if (Image[i * W + j] == 255) // 전경화소라면
+			{
+				if (!(Image[(i - 1) * W + j] == 255 &&
+					Image[(i + 1) * W + j] == 255 &&
+					Image[i * W + (j + 1)] == 255 &&
+					Image[i * W + (j - 1)] == 255)) { // 4주변화소가 모두 전경화소가 아니라면
+					Output[i * W + j] == 0;
+				}
+				else Output[i * W + j] = 255;
+			}
+			else Output[i * W + j] = 0;
+		}
+	}
+}
+
 int main()
 {
 	BITMAPFILEHEADER hf; 
 	BITMAPINFOHEADER hInfo; 
 	RGBQUAD hRGB[256];
 	FILE* fp;
-	fp = fopen("face.bmp", "rb");
+	fp = fopen("erosion.bmp", "rb");
 	if (fp == NULL) {
 		printf("File not found!\n");
 		return -1;
@@ -790,6 +809,14 @@ int main()
 	free(Cb);
 	free(Cr);
 	*/
+
+	Erosion(Image, Output, W, H);
+	Erosion(Output, Image, W, H);
+	Erosion(Image, Output, W, H);
+	Erosion(Output, Image, W, H);
+	Erosion(Image, Output, W, H);
+	Erosion(Output, Image, W, H);
+	Erosion(Image, Output, W, H);
 
 	SaveBMPFile(hf, hInfo, hRGB, Output, W, H, "output.bmp");
 
